36e84600bc7d6a8d97477b7ee60eba875b8508a2
FREDERICK <github@FREDERICK.win>
2021-03-28 03:16:21 +0200

    Misc

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-//declarin' vars
 var horn = $("audio")[0];
 var buttonWrapper = $(".buttonWrapper");
 
-//WATCHIN', WAITIN' TO GO HAM
-//click faster for more noscope headshots
 buttonWrapper.mousedown(function() {
   horn.currentTime = 0; //reset play time if re-clicked
   horn.play(); //GO HAM
